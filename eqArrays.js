82d417290aa2d49c49fc2b86d02fe444dadb1303
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-10-26 14:56:26 -0400

    refactored eqArrays ftn

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,9 @@
 function eqArrays(arr1, arr2) {
   let match = true;
-
-  //loop through first array
   for (let i = 0; i < arr1.length; i++) {
-    //loop through second array
-    for (let j = i; j === i; j++) {
-      //take the element of array 1 at position 1 and  the element of array 2 at the same position and compares the two, if the two values don't match, match variable changes to false and the loop is exited (for efficiency)
-      if (arr1[i] !== arr2[j]) {
-        match = false;
-        break;
-      }
+    if (arr1[i] !== arr2[i]) {
+      match = false;
+      break;
     }
   }
   return match;
