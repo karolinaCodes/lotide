bf19d1e043f27ec4f7bebc5961362f0112943416
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-10-26 20:17:51 -0400

    add countOnly implmentation

---
+++ countOnly.js
@@ -0,0 +1,46 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function (allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    //only increment our count in result if the item is found in the itemsToCount object
+    if (itemsToCount[item]) {
+      //if the property on the result obj exists increase the count, if it doesn't add that property with the value of 1
+      results[item] ? results[item]++ : (results[item] = 1);
+    }
+  }
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
